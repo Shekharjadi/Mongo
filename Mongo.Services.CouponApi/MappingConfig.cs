3b97d4675e41898b0290c6d2fa500f4eca581f9b
Mande P <mande20252025@outlook.com>
2025-12-05 06:14:12 +0000

    Add project files.

---
+++ Mongo.Services.CouponApi/MappingConfig.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Mango.Services.CouponAPI.Models;
+using Mango.Services.CouponAPI.Models.Dto;
+
+namespace Mango.Services.CouponAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(cfg =>
+            {
+                // Register mappings here
+                cfg.CreateMap<CouponDto, Coupon>();
+                cfg.CreateMap<Coupon, CouponDto>();
+            });
+
+            return mappingConfig;
+        }
+    }
+}
