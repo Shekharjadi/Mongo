3eb686cd9c518b24899a2239ef7e46365bf521d5
Mande P <mande20252025@outlook.com>
2025-12-06 09:25:39 +0000

    added secretes in controllers, configuration files, environment files, views, JavaScript, CSS,  and miscellaneous text files

---
+++ Mongo-web/wwwroot/js/fake-secrets.js
@@ -0,0 +1,11 @@
+﻿// Fake Firebase API Key
+const firebaseKey = "AIzaSyFakeFirebaseKey-1234567890abcdef";
+
+// Fake Mapbox key
+const mapboxKey = "pk.eyJ1IjoiZmFrZXVzZXIiLCJhIjoiY2tzb2Zha2VrZXkifQ.fake";
+
+// Fake reCAPTCHA key
+const recaptchaKey = "6LeIxFAAAAAAChqRbQZCN-FAKE_RECAPTCHA_SITE_KEY";
+
+// Fake Auth0 Token
+const auth0Token = "eyJhbGciOiJSUzI1NiIsImtpZCI6IkF1dGgwRG9jdW1lbnQifQ.fakepayload.1234567890";
