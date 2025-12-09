5ffd572af3b3a7258d6d60b09293501a452da5b7
Mande P <mande20252025@outlook.com>
2025-12-09 06:25:28 +0000

    added  new secretes

--- Mongo.Services.CouponApi/Controllers/SecretController.cs
+++ Mongo.Services.CouponApi/Controllers/SecretController.cs
@@ -1,37 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Mongo.Services.CouponApi.Controllers;  // Make sure this is the correct namespace for SecretService
 
-namespace SecretLeakWebAPI.Controllers
+namespace Mongo.Services.CouponApi.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
-    public class SecretController : ControllerBase
+    [Route("api/[controller]")]
+    public class SecretsController : ControllerBase
     {
-        // Intentionally exposed secrets
-        private const string API_KEY = "API_KEY_1234567890abcdef9999999999";
-        private const string JWT_SECRET = "JWT_SECRET_xxyyzz112233445566778899";
-        private const string DB_PASSWORD = "dbPassword!@#1234567890";
+        private readonly SecretService _service;
 
-        private readonly string AwsAccessKey = "AKIAFAKEKEY123456789";
-        private readonly string AwsSecretKey = "FakeAWSSecretKey99999999999999";
-        private readonly string StripeKey = "sk_test_FAKE_STRIPE_KEY_ABC123";
-        private readonly string PrivateKeyBlock =
-@"-----BEGIN PRIVATE KEY-----
-MIIBVgIBADANBgkqhkiG9FAKEKEY...AAAAAA
------END PRIVATE KEY-----";
-
-        [HttpGet]
-        public IActionResult GetSecrets()
+        // Constructor to inject SecretService
+        public SecretsController(SecretService service)
         {
-            return Ok(new
+            _service = service;
+        }
+
+        [HttpGet("dummy")]
+        public IActionResult GetDummySecrets()
+        {
+            var secrets = _service.GetAllSecrets();  // Get the secrets from the service
+
+            if (secrets == null)
             {
-                apiKey = API_KEY,
-                jwt = JWT_SECRET,
-                database = DB_PASSWORD,
-                awsAccessKey = AwsAccessKey,
-                awsSecretKey = AwsSecretKey,
-                stripeKey = StripeKey,
-                privateKey = PrivateKeyBlock
-            });
+                return NotFound("No secrets found.");
+            }
+
+            return Ok(secrets);  // Return the secrets as a response
         }
     }
 }
---
+++ Mongo.Services.CouponApi/Controllers/SecretService.cs
@@ -0,0 +1,16 @@
+﻿namespace Mongo.Services.CouponApi.Controllers
+{
+    public class SecretService
+    {
+        public object GetAllSecrets()
+        {
+            return new
+            {
+                AwsAccessKey = "AKIA123456789TESTKEY",
+                GithubToken = "ghp_1234567890DUMMYTOKEN1234567890ABCD",
+                SlackToken = "xoxb-123456789012-DUMMYTOKEN",
+                JwtToken = "eyJhbGciOiJIUzI1NiJ9.DUMMY.DUMMY"
+            };
+        }
+    }
+}
