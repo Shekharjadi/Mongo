b32cd81f4123f1bc0feb6135d35ce8f3286d7be6
Mande P <mande20252025@outlook.com>
2025-12-11 17:04:55 +0000

    added secrete scanner

--- Mongo-web/Controllers/HomeController.cs
+++ Mongo-web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Mongo_web.Models;
+using Newtonsoft.Json;
 using System.Diagnostics;
 
 namespace Mongo_web.Controllers
@@ -14,7 +16,23 @@ namespace Mongo_web.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            // Use relative path to access the file in wwwroot
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Secrets", "gitleaks_report.json");
+
+            // Check if the file exists
+            if (!System.IO.File.Exists(filePath))
+            {
+                return Content("The JSON file does not exist at the specified location.");
+            }
+
+            // Read the file content
+            var jsonContent = System.IO.File.ReadAllText(filePath);
+
+            // Deserialize the JSON content into a list of GitleaksResult objects
+            var results = JsonConvert.DeserializeObject<List<GitleaksResult>>(jsonContent);
+
+            // Pass the data to the view
+            return View(results);
         }
 
         public IActionResult Privacy()
---
+++ Mongo-web/Models/GitleaksResult.cs
@@ -0,0 +1,21 @@
+﻿namespace Mongo_web.Models
+{
+    public class GitleaksResult
+    {
+        public string RuleID { get; set; }
+        public string Description { get; set; }
+        public int StartLine { get; set; }
+        public int EndLine { get; set; }
+        public string Match { get; set; }
+        public string Secret { get; set; }
+        public string File { get; set; }
+        public string Commit { get; set; }
+        public string Link { get; set; }
+        public string Author { get; set; }
+        public string Email { get; set; }
+        public string Date { get; set; }
+        public string Message { get; set; }
+        public string Fingerprint { get; set; }
+    }
+
+}
